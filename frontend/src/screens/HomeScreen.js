9026be616dc92b4af579ce648d53830a3cb5996e
laminsurachman <rahman0669@gmail.com>
2024-08-14 17:35:59 +0700

    Home screen product list add

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import products from "../products";
 import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
 function HomeScreen() {
   return (
     <div>
@@ -8,7 +9,7 @@ function HomeScreen() {
       <Row>
         {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3}>
-            <h3>{product.name}</h3>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
